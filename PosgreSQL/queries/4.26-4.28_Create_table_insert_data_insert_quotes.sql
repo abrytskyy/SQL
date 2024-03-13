c14e375d345c83daec16164058ebb40b0c761edd
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2024-03-13 14:35:36 +0100

    4.26-4.28_Create_table_insert_data_insert_quotes.sql

---
+++ PosgreSQL/queries/4.26-4.28_Create_table_insert_data_insert_quotes.sql
@@ -0,0 +1,33 @@
+CREATE TABLE customers (
+	customer_id SERIAL PRIMARY kEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(150),
+	age INT
+);
+
+SELECT * FROM customers;
+
+
+
+INSERT INTO customers (first_name, last_name, email, age)
+VALUES ('Artem', 'Brytskyy', 'abrytskyy2@gmail.com', 44);
+
+SELECT * FROM customers;
+
+
+
+INSERT INTO customers (first_name, last_name)
+VALUES
+('John', 'Adams'),
+('Linda', 'Abe');
+
+SELECT * FROM customers;
+
+
+
+INSERT INTO customers (first_name)
+VALUES
+('Bill''O Sullivan');
+
+SELECT * FROM customers;
