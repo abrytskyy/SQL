5ebe2f0ac6f7e5237dfb79c073b44776c20c1ffc
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2024-03-14 15:40:53 +0100

    5.44-DISTINCT.sql

---
+++ PosgreSQL/queries/5.44-DISTINCT.sql
@@ -0,0 +1,13 @@
+SELECT movie_lang FROM movies; 
+
+SELECT DISTINCT movie_lang FROM movies; 
+
+
+
+SELECT DISTINCT movie_lang, director_id FROM movies
+ORDER BY 1; 
+
+
+
+SELECT DISTINCT * FROM movies
+ORDER BY 1; 
