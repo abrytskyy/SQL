2d1072dcf475eca2cd1902561849f5ef4fd31553
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2024-03-16 08:13:25 +0100

    6.54-LIMIT_OFFSET.sql

---
+++ PosgreSQL/queries/6.54-LIMIT_OFFSET.sql
@@ -0,0 +1,31 @@
+SELECT * FROM movies 
+ORDER BY 3 DESC
+LIMIT 5
+
+SELECT * FROM directors 
+WHERE nationality = 'American'
+ORDER BY 4
+LIMIT 5
+
+SELECT * FROM actors
+WHERE gender = 'F'
+ORDER BY 5 DESC
+LIMIT 10
+
+SELECT * FROM movies_revenues
+ORDER BY 3 DESC NULLS LAST
+LIMIT 10
+
+SELECT * FROM movies_revenues
+ORDER BY 3 
+LIMIT 10
+
+
+	
+SELECT * FROM movies
+ORDER BY 1 
+LIMIT 5 OFFSET 3
+
+SELECT * FROM movies_revenues
+ORDER BY 3 DESC NULLS LAST
+LIMIT 5 OFFSET 5
