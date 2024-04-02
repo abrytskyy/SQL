eaa43316ae652547346e2e02517969257e2f5682
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2024-04-02 14:37:16 +0200

    4.15-aliases.sql

--- MySQL/exercises/4.15-aliases.sql
+++ MySQL/exercises/4.15-aliases.sql
@@ -1,2 +1,4 @@
 USE sakila;
 SELECT first_name, last_name, CONCAT(first_name, ' ', last_name) full_name FROM actor; 
+
+SELECT first_name , last_name "Family Name", CONCAT(first_name, ' ', last_name) "Full Name" FROM actor;  
