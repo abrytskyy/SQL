af47ad5ef6725e8ff94dd106b178f3673be0ad2c
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2024-08-28 19:35:53 +0200

    Update solutions.sql

--- 15days/day4-day6/solutions.sql
+++ 15days/day4-day6/solutions.sql
@@ -32,3 +32,46 @@ select '***' ||
 		SUBSTRING (email from POSITION('.' in email)-1 for 3)|| '***' ||
 		SUBSTRING (email from POSITION('@' in email))
 from customer
+
+select * from payment
+
+select EXTRACT(month from payment_date) as month, sum(amount) from payment
+group by 1
+order by 2 desc
+
+select EXTRACT(dow from payment_date) day_of_week, sum(amount) from payment
+group by 1
+order by 2 desc
+
+select EXTRACT(week from payment_date) week, customer_id, sum(amount) from payment
+group by 1, 2
+order by 3 desc
+
+
+select sum(amount) total_amount, to_char(payment_date, 'Dy, DD/MM/YYYY' )
+from payment
+group by 2
+order by 1
+
+select sum(amount) total_amount, to_char(payment_date, 'Mon, YYYY' )
+from payment
+group by 2
+order by 1
+
+select sum(amount) total_amount, to_char(payment_date, 'Dy, HH:MI' )
+from payment
+group by 2
+order by 1 desc
+
+SELECT CURRENT_DATE, CURRENT_TIMESTAMP,
+rental_date
+FROM rental
+
+SELECT return_date-rental_date rental_duration
+FROM rental
+where customer_id = 35
+
+SELECT customer_id, avg(return_date-rental_date) avg_rental_duration
+FROM rental
+group by 1
+order by 2 desc
