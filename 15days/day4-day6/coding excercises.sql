3e1c2c16efa4e808aa5d514410f846d6c9bc7e2e
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2024-09-18 10:22:27 +0200

    Create coding excercises.sql

---
+++ 15days/day4-day6/coding excercises.sql
@@ -0,0 +1,10 @@
+--Coding Exercise 1: Let's get started!	
+SELECT * FROM payment
+
+--Coding Exercise 2: SELECT
+select title, publication_year
+from books
+
+--Coding Exercise 3: SELECT & ORDER BY
+select * from books
+order by price desc
