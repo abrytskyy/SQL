196074c0a003389978c68d6709aa51a04a2251da
abrytskyy <87816044+abrytskyy@users.noreply.github.com>
2024-09-18 10:30:19 +0200

    Update coding excercises.sql

--- 15days/day4-day6/coding excercises.sql
+++ 15days/day4-day6/coding excercises.sql
@@ -1,10 +1,37 @@
---Coding Exercise 1: Let's get started!	
-SELECT * FROM payment
+--Coding Exercise 16: LENGTH, LOWER & UPPER
+select review_id, review_text, length(review_text) review_length
+from customer_reviews
+where product_id = 101 and lower(review_text) like '%great%' 
 
---Coding Exercise 2: SELECT
-select title, publication_year
-from books
+--Coding Exercise 17: Concatenate
+select name || ' - '|| category || ': $' || price product_summary from products
+order by name
 
---Coding Exercise 3: SELECT & ORDER BY
-select * from books
-order by price desc
+--Coding Exercise 18: Mathematical functions and operators
+select product_name, sum(quantity_sold * price_per_unit)total_revenue from sales
+group by 1
+order by 2 desc
+limit 1
+
+--Coding Exercise 19: CASE WHEN
+select order_id, product_id, quantity, unit_price, 
+case
+when quantity > 1 then quantity*unit_price*0.9+shipping_fee
+else quantity*unit_price+shipping_fee
+end total_price
+from sales_orders
+
+--Coding Exercise 20: CASE WHEN & SUM
+select sum(case
+when category = 'Income' then amount end) TotalIncome,
+sum(case
+when category = 'Expense' then amount end) TotalExpenses,
+sum(case
+when category = 'Income' then amount end) -
+sum(case
+when category = 'Expense' then amount end) NetIncome
+from transactions
+
+--Coding Exercise 21: COALESCE
+SELECT transaction_id, account_id, transaction_type, amount, coalesce(description, 'Not Provided') description
+from transactions
